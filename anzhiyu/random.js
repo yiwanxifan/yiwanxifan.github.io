f4be79aa73cf8ac2eaa948941f5b2d4e9cdc7d9a
Dylan chen <908409990@qq.com>
2023-05-13 17:27:05 +0800

    Site updated: 2023-05-13 17:27:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/f213c33e.html","posts/9a18fb74.html","posts/6a05b400.html","posts/45418e2c.html","posts/9d2d750f.html","posts/7a6a6b5a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/9a18fb74.html","posts/f213c33e.html","posts/45418e2c.html","posts/7a6a6b5a.html","posts/9d2d750f.html","posts/6a05b400.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
